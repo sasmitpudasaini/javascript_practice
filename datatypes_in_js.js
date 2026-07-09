16a7bfb2c9ddefeaf9e689db3035e5143c29aa5d
Sasmit Pudasaini <sasmitpudasaini@gmail.com>
2026-07-09 12:35:20 +0545

    first push

---
+++ datatypes_in_js.js
@@ -0,0 +1,10 @@
+let a = 1
+let b = "yoyo"
+let c = null
+let d = BigInt("123") + BigInt("5")
+let e = Symbol("i am who i am")
+let f 
+
+console.log(a,b,c,d,e,f)
+console.log(typeof b)
+console.log(typeof e)
---
+++ variables.js
@@ -0,0 +1,9 @@
+let a = 12
+console.log(a)
+
+var b ="hello"
+console.log(b)
+
+const z = 123
+console.log(z)
+

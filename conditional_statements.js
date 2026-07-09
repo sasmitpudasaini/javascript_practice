ae31fb296119212632c69e1cda1a1f676491864c
Sasmit Pudasaini <sasmitpudasaini@gmail.com>
2026-07-09 16:20:54 +0545

    push

---
+++ conditional_statements.js
@@ -0,0 +1,18 @@
+let a = 5
+let b = 10
+let c = a+b
+console.log(c)
+
+if(c>14){
+    console.log("it's is more than 14")
+}
+else if(c<14){
+    console.log("it's is less than 14")
+}
+else if(c == 14){
+    console.log("it's equals 14")
+}
+else{
+    console.log("it's too less")
+}
+
---
+++ switchcase.js
@@ -0,0 +1,33 @@
+let day = "friday"
+
+switch(day) {
+    case "monday":
+        console.log("shit!! back to work")
+        break;
+    case "wednesday":
+        console.log("come on half way there")
+        break;
+    case "friday":
+        console.log("it's here it's here")
+        break;
+    default:
+        console.log("just another day")
+}
+
+
+let food = "chilli"
+switch(food){
+    case "chocolate":
+        console.log("it's sweet")
+        break;
+    case "chilli":
+        console.log("fire!! outta this world")
+        break;
+    case "lemon":
+        console.log("its crazy")
+        break;
+    default:
+        console.log("its just a food")
+        
+
+}

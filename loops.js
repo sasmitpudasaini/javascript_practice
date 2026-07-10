f0dd42c61537d5239949b0ae7a248e830fecde5d
Sasmit Pudasaini <sasmitpudasaini@gmail.com>
2026-07-10 11:44:09 +0545

    push

--- loops.js
+++ loops.js
@@ -3,3 +3,10 @@ for (i = 1; i<=10; i++){
     console.log(i)
 }
 
+//sum of first five natural number
+let sum = 0
+let n = 5
+for ( i = 1; i<=n; i++){
+}
+console.log("sum of first " +n+ " natural number is " +(sum += i))
+
